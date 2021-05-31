92f130979a9dc060b3c13bf90a9e0b25c86a677e
Roman Kavinskyi <mrronanx@gmail.com>
2021-05-31 19:33:30 +0700

    Initial Commit

---
+++ SMART Planning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SMART Planning
+//
+//  Created by Roman Kavinskyi on 5/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SMART Planning/SMART_PlanningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMART_PlanningApp.swift
+//  SMART Planning
+//
+//  Created by Roman Kavinskyi on 5/31/21.
+//
+
+import SwiftUI
+
+@main
+struct SMART_PlanningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
